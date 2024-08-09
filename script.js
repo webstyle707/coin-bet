a01a6f17acac868c7e8d6f608e85246f4af88b52
webstyle707 <webstyle707@gmail.com>
2024-08-09 16:12:13 +0800

    update

--- script.js
+++ script.js
@@ -5,7 +5,60 @@ function url() {
   }
 
 
-  let menu;
-  const showmenu = ()=>{
-    
+
+  let scale1 = document.getElementById('scale1').style
+let scale2 = document.getElementById('scale2').style
+let scale3 = document.getElementById('scale3').style
+let scale4 = document.getElementById('scale4').style
+let scale5 = document.getElementById('scale5').style
+let scale6 = document.getElementById('scale6').style
+
+  const handlehover1 = ()=>{
+    scale1.transform = "scale(1.05)"
   }
+  const handleclearhover1 = ()=>{
+      scale1.transform = "scale(1)"
+  }
+
+  const handlehover2 = ()=>{
+    scale2.transform = "scale(1.05)"
+  }
+  const handleclearhover2 = ()=>{
+      scale2.transform = "scale(1)"
+  }
+
+
+  const handlehover3 = ()=>{
+    scale3.transform = "scale(1.05)"
+  }
+  const handleclearhover3 = ()=>{
+      scale3.transform = "scale(1)"
+  }
+
+
+
+  const handlehover4 = ()=>{
+    scale4.transform = "scale(1.05)"
+  }
+  const handleclearhover4 = ()=>{
+      scale4.transform = "scale(1)"
+  }
+
+
+
+  const handlehover5 = ()=>{
+    scale5.transform = "scale(1.05)"
+  }
+  const handleclearhover5 = ()=>{
+      scale5.transform = "scale(1)"
+  }
+
+
+
+  const handlehover6 = ()=>{
+    scale6.transform = "scale(1.05)"
+  }
+  const handleclearhover6 = ()=>{
+      scale6.transform = "scale(1)"
+  }
+
